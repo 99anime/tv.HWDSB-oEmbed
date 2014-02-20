df6d37c12b4f28cdf2ea9bb43ae9b39269542cb4
mrjarbenne <jarbenne@hwdsb.on.ca>
2014-02-19 19:40:21 -0500

    Initial upload

---
+++ oembeds.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: Additional OEmbed Providers
+Description: As the name suggests
+Version: 0.1
+Author: mrjarbenne
+Author URI: http://mrjarbenne.ca
+License: GPLv2 or later
+*/
+
+/*
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+add_action( 'init', 'add_oembed_tvhwdsb' );
+
+function add_oembed_tvhwdsb() {
+wp_oembed_add_provider( 'https://tv.hwdsb.on.ca/id.*', 'https://tv.hwdsb.on.ca/oembed', true );
+}	
